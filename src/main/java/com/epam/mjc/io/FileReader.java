60f7e21f638ecccc2bd307afe515e9eb4ddad520
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-22 20:02:56 +0500

    updated FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -16,22 +16,14 @@ public class FileReader {
                 if (s.contains("Name")) {
                     profile.setName(s.substring(delimiter + 2));
                 } else if (s.contains("Age")) {
-                    try {
-                        profile.setAge(Integer.parseInt(s.substring(delimiter + 2)));
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
+                    profile.setAge(Integer.parseInt(s.substring(delimiter + 2)));
                 } else if (s.contains("Email")) {
                     profile.setEmail(s.substring(delimiter + 2));
                 } else {
-                    try {
-                        profile.setPhone(Long.parseLong(s.substring(delimiter + 2)));
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
+                    profile.setPhone(Long.parseLong(s.substring(delimiter + 2)));
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | NumberFormatException e) {
             e.printStackTrace();
         }
         return new Profile(profile.getName(), profile.getAge(), profile.getEmail(), profile.getPhone());
